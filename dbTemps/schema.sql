36976621f4fe14e72598b8b7426db0543c11089a
Trent Schneider <trentonschneider@gmail.com>
2020-08-29 11:04:36 -0400

    adding starter db tables to schema

---
+++ dbTemps/schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS wiki_db;
+CREATE DATABASE wiki_db;
+USE wiki_db;
+
+CREATE TABLE wikis (
+id int NOT NULL AUTO_INCREMENT,
+category VARCHAR (35),
+title VARCHAR (100),
+description VARCHAR (255),
+userID INT (4),
+FOREIGN KEY (userID) references users(id),
+PRIMARY KEY(id)
+);
+
+CREATE TABLE users (
+id int NOT NULL AUTO_INCREMENT,
+name VARCHAR (20),
+password VARCHAR (20),
+PRIMARY KEY(id)
+);
