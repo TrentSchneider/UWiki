3361732392d77d9899b1bdad410fcde3de53b438
Trent Schneider <trentonschneider@gmail.com>
2020-09-02 19:14:18 -0400

    updated future login routes to work with handlebars and removed routes that don't work with current app

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -35,22 +35,16 @@ router.get("/about", (req, res) => {
   res.render("about");
 });
 
-// router.get("/", (req, res) => {
-//   // If the user already has an account send them to the members page
-//   if (req.user) {
-//     res.redirect("/members");
-//   }
-//   res.sendFile(path.join(__dirname, "../public/signup.html"));
-// });
-
 // router.get("/login", (req, res) => {
 //   // If the user already has an account send them to the members page
 //   if (req.user) {
-//     res.redirect("/members");
+//     res.redirect("/");
 //   }
-//   res.sendFile(path.join(__dirname, "../public/login.html"));
+// // will switch out index when login form handlebars added
+//   res.render("/index");
 // });
 
+// // The isAuthenticated can be added to the home page route so that handlebars can pick whether they are able to create/edit pages or not
 // // Here we've add our isAuthenticated middleware to this route.
 // // If a user who is not logged in tries to access this route they will be redirected to the signup page
 // router.get("/members", isAuthenticated, (req, res) => {
