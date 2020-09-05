fa9de9e83730cd4bf99f2bda93ab7027748f04ef
TrentSchneider <trentonschneider@gmail.com>
2020-09-05 13:30:16 -0400

    updated db schema and seed

--- dbTemps/seeds.sql
+++ dbTemps/seeds.sql
@@ -1,5 +1,5 @@
--- Seed for wikis table (needs to be entered first to line up with users table seed below)
-INSERT INTO wikis (category, title, description, userID) VALUES ('Code', "Java Script", "Basic overview of Java Script", "as;ldkfasdlkfsa;f a;sdlkjas;dlkfjs a;dslfjasd;lfkjasd sal;kdfjas;lkdjfa", 1);
-
--- Seed for users table (needs to be entered first to line up with wikis table seed above)
+-- Seed for users table (needs to be entered first to line up with wikis table seed)
 INSERT INTO users (name, email, password) VALUES ("Bob", "bob@email.com", "asdf");
+
+-- Seed for wikis table (needs to be entered second to line up with users table seed)
+INSERT INTO wikis (category, title, description, userID) VALUES ("Code", "Java Script", "(test data) as;ldkfasdlkfsa;f a;sdlkjas;dlkfjs a;dslfjasd;lfkjasd sal;kdfjas;lkdjfa", 1);
