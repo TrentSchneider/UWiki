5f0390f7ac2306c1cb13941b48482261ab6ef81f
LisaSuv <63619744+purilisa@users.noreply.github.com>
2020-09-09 21:47:54 -0400

    adding jquery

---
+++ public/js/viewWiki.js
@@ -0,0 +1,22 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+
+$(document).ready(() => {
+  // Getting references to our form and inputs
+  const viewWikiLink = $(".collection-item"); //Link to view teh wiki description page
+
+  viewWikiLink.on("click", (event) => {
+    event.preventDefault();
+    const id = $(this).data("id");
+
+    $.ajax("/view/:id", {
+      type: "GET",
+      data: id,
+    })
+      .then(() => {
+        window.location.replace("/view/:id");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+});
